fc0aa3c1a2f974f41a2fce5486590a189f55506c
Siddharth Kumar <kumarsiddharth166@gmail.com>
2026-07-19 12:59:45 +0530

    fix: make serve.js robust with catch-all routes

--- dashboard/serve.js
+++ dashboard/serve.js
@@ -4,9 +4,24 @@ const path=require('path');
 
 const app=express();
 
-app.use(express.static(__dirname));
+// Serve all static files (css, js, html, images)
+app.use(express.static(path.join(__dirname)));
 
+// Explicit routes for all pages
 app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname,'index.html'));
+});
+
+app.get('/dashboard',(req,res)=>{
+  res.sendFile(path.join(__dirname,'dashboard.html'));
+});
+
+app.get('/register',(req,res)=>{
+  res.sendFile(path.join(__dirname,'register.html'));
+});
+
+// Catch-all: send back to login
+app.get('*',(req,res)=>{
   res.sendFile(path.join(__dirname,'index.html'));
 });
 
